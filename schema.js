e965f90a87fa3e3210de248226b2394b60968493
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2019-05-06 16:23:44 +0200

    Create RootQuery

--- schema.js
+++ schema.js
@@ -1,8 +1,12 @@
+const axios = require("axios");
+
 const {
 	GraphQLObjectType,
 	GraphQLInt,
 	GraphQLString,
-	GraphQLBoolean
+	GraphQLBoolean,
+	GraphQLList,
+	GraphQLSchema
 } = require("graphql");
 
 //Launch Type
@@ -26,4 +30,34 @@ const RocketType = new GraphQLObjectType({
 		rocket_name: { type: GraphQLString },
 		rocket_type: { type: GraphQLString }
 	})
+});
+
+// Root Query
+const RootQuery = new GraphQLObjectType({
+	name: "RootQueryType",
+	fields: {
+		launches: {
+			type: new GraphQLList(LaunchType),
+			resolve(parent, args) {
+				return axios
+					.get("https://api.spacexdata.com/v3/launches")
+					.then(res => res.data);
+			}
+		},
+		launch: {
+			type: LaunchType,
+			args: {
+				flight_number: { type: GraphQLInt }
+			},
+			resolve(parent, args) {
+				return axios
+					.get(`https://api.spacexdata.com/v3/launches/${args.flight_number}`)
+					.then(res => res.data);
+			}
+		}
+	}
+});
+
+module.exports = new GraphQLSchema({
+	query: RootQuery
 });
